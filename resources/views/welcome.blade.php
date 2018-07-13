cbd4fc9d08cd013a72dfbf9c7989f5b16dd5a300
ZcZmm <18735135833@163.com>
2018-07-13 16:18:28 +0800

    Signed-off-by: ZcZmm <18735135833@163.com>修改了主页面内容

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by zhangchao</div>
+                <div class="title">Hello Laravel! - by ZhangChao</div>
             </div>
         </div>
     </body>
